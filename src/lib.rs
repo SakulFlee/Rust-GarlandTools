0e9a2521114cf8a3f12100d3324c82b61052d780
Lukas Weber <me@sakul6499.de>
2022-09-25 00:23:08 +0200

    Linking it all together in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,17 @@ extern crate lazy_static;
 #[cfg(test)]
 mod tests;
 
+#[macro_use]
+mod test_utils;
+
 // Modules
-pub mod globals;
+mod garland_tools;
+mod globals;
+mod jobs;
+mod languages;
 
 // Reexports
+pub use crate::garland_tools::GarlandTools;
+pub use crate::globals::*;
+pub use crate::jobs::Job;
+pub use crate::languages::Language;
