c43e06e3577936e044ecd8101fd168f5388f2d9c
Lukas Weber <me@sakul6499.de>
2022-09-25 19:25:19 +0200

    Updated tests for language enum

--- src/tests/test_languages.rs
+++ src/tests/test_languages.rs
@@ -1,27 +1,23 @@
 use crate::Language;
 
 #[test]
-fn test_language_new_en_works() {
-    let language = Language::new("en".to_string());
-    assert_eq!(language, Language::ENGLISH);
+fn test_language_english() {
+    assert_eq!(Language::ENGLISH.get_language(), "en");
 }
 
 #[test]
-fn test_language_new_de_works() {
-    let language = Language::new("de".to_string());
-    assert_eq!(language, Language::GERMAN);
+fn test_language_de() {
+    assert_eq!(Language::GERMAN.get_language(), "de");
 }
 
 #[test]
-fn test_language_new_fr_works() {
-    let language = Language::new("fr".to_string());
-    assert_eq!(language, Language::FRENCH);
+fn test_language_new_french() {
+    assert_eq!(Language::FRENCH.get_language(), "fr");
 }
 
 #[test]
-fn test_language_new_ja_works() {
-    let language = Language::new("ja".to_string());
-    assert_eq!(language, Language::JAPANESE);
+fn test_language_japanese() {
+    assert_eq!(Language::JAPANESE.get_language(), "ja");
 }
 
 #[test]
